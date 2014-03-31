b62a908d469ec48c110324829086f5346418034e
Martin <hongzeqin@gmail.com>
2014-04-01 00:36:39 +0800

    enable to get current time

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -4,8 +4,12 @@ var parseArgs = require("minimist")
     , createMiniHarp = require("mini-harp")
     , app = createMiniHarp();
 
-var argv = parseArgs(process.argv);
+// use a current time middleware
+app.use(function(request, response, next) {
+  request.url == "/current-time" ? response.end((new Date()).toISOString()) : next();
+});
 
+var argv = parseArgs(process.argv);
 var port = argv.port || 4000;
 
 app.listen(port);
