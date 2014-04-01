e96ac84ecc09573a03c86972be16bf22d21ee8cc
Martin <hongzeqin@gmail.com>
2014-04-02 01:15:15 +0800

    root-as-index

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ module.exports = function(root) {
   var connect       = require('connect')
       , serveStatic = require('serve-static')
       , makeJade    = require('./lib/processor/jade')
-      , makeLess    = require('./lib/processor/less');
+      , makeLess    = require('./lib/processor/less')
+      , rewrite     = require('./lib/processor/rewrite');
 
   app = connect()
           .use(serveStatic(root))
           .use(function(request, response, next) {
             request.url == "/current-time" ? response.end((new Date()).toISOString()) : next();
           })
+          .use(rewrite)
           .use(makeJade(root))
           .use(makeLess(root));
 
