720bada6d5c35b743ad58e54852171946f0be3d9
Martin <hongzeqin@gmail.com>
2014-04-02 00:53:27 +0800

    less preprocessor

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 module.exports = function(root) {
   var connect       = require('connect')
       , serveStatic = require('serve-static')
-      , makeJade    = require('./lib/processor/jade');
+      , makeJade    = require('./lib/processor/jade')
+      , makeLess    = require('./lib/processor/less');
 
   app = connect()
           .use(serveStatic(root))
           .use(function(request, response, next) {
             request.url == "/current-time" ? response.end((new Date()).toISOString()) : next();
           })
-          .use(makeJade(root));
+          .use(makeJade(root))
+          .use(makeLess(root));
 
   return app;
 }
