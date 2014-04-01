5e010382f19ae512f46a1c31ddfb70ca8728faee
Martin <hongzeqin@gmail.com>
2014-04-02 01:56:41 +0800

    reject stupid requests

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 module.exports = function(root) {
-  var connect       = require('connect')
-      , serveStatic = require('serve-static')
-      , makeJade    = require('./lib/processor/jade')
-      , makeLess    = require('./lib/processor/less')
-      , rewrite     = require('./lib/processor/rewrite');
+  var connect        = require('connect')
+      , serveStatic  = require('serve-static')
+      , makeJade     = require('./lib/processor/jade')
+      , makeLess     = require('./lib/processor/less')
+      , rewrite      = require('./lib/processor/rewrite')
+      , rejectStupid = require('./lib/processor/rejectStupid');
 
   app = connect()
+          .use(rejectStupid)
           .use(serveStatic(root))
           .use(function(request, response, next) {
             request.url == "/current-time" ? response.end((new Date()).toISOString()) : next();
